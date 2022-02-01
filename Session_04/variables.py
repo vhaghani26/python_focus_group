ea5538203f05d0b86e810d45489d5d9e95791306
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-02-01 15:46:33 -0800

    work on python variables

---
+++ Session_04/variables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+# Illegal variable names
+# 1animal = "Snake"
+# an-animal = "Dog"
+# an animal = "Cat"
+
+# Legal variable names
+ananimal = "Budgie"
+an_animal = "Iguana"
+_an_animal = "Gecko"
+anAnimal = "Owl"
+AnAnimal = "Fish"
+ANANIMAL = "Bunny"
+an_animal_1 = "Bear"
+
+# Print with quotes
+print("an_animal_1")
+# Printing a variable
+print(an_animal_1)
+
+# Example variables
+my_var = 3
+# Show that variable gets overwritten
+my_var = 5
+
+print(my_var)
