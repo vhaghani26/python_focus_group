366ababc44fc443f6bbe6a1e3781ba96256f95e1
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-02-01 15:46:41 -0800

    learn about comment types

---
+++ Session_04/comments.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+Script Usage:
+To run comments.py, use the command "python3 comments.py" at the command line.
+
+Output:
+The output of comments.py are miscellaneous statements used as arguments to demonstrate the print() function
+'''
+
+print("Here is an example of an inline comment") # Inline comment
+
+# Print my name
+print("Viki")
+
+# Prints numbers 1-20
+for i in range(1,21):
+	print(i)
+
+# Prints the names of all my pets	
+pet_names = ["Stella", "Blueberry", "Sunny", "Bird", "Zoscia", "New Pet"]
+for pet in pet_names:
+	print(pet)
