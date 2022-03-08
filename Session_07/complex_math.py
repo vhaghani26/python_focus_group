1dfc0df26bea294d85c4b2b2e94dd57b5212f278
Viktoria Haghani <62615778+vhaghani26@users.noreply.github.com>
2022-03-08 15:56:42 -0800

    demonstrate import module

---
+++ Session_07/complex_math.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import math
+
+# Use square root for Pythagorean theorem
+a = 3
+b = 4
+
+hyp = math.sqrt(a**2 + b**2)
+print(hyp)
+
+# Use log2
+print(math.log2(0.25))
+
+# Use pi
+print(math.pi)
+
+# Use Euler's number
+print(math.e)
+
+# Use infinity 
+print(math.inf)
+
+# Use factorial
+print(math.factorial(a))
