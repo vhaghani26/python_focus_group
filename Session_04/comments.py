4c3ef6aca0ecb3552cb9dbe4e5cc5e6d37220044
Viktoria Haghani <vhaghani@ucdavis.edu>
2026-03-18 09:35:38 -0700

    add variables

--- Session_04/comments.py
+++ Session_04/comments.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 
-'''
-Script Usage:
-To run comments.py, use the command "python3 comments.py" at the command line.
 
-Output:
-The output of comments.py are miscellaneous statements used as arguments to demonstrate the print() function
-'''
+ananimal = "Budgie"
+an_animal = "Iguana"
+_an_animal = "Gecko"
+anAnimal = "Owl"
+AnAnimal = "Fish"
+ANANIMAL = "Bunny"
+an_animal_1 = "Guinea Pig"
 
-print("Here is an example of an inline comment") # Inline comment
-
-# Print my name
-print("Viki")
-
-
-# Prints numbers 1-20
-for i in range(1,21):
-	print(i)
-    
-# Prints the names of all my pets	
-pet_names = ["Stella", "Blueberry", "Sunny", "Bird", "Zoscia", "New Pet"]
-for pet in pet_names:
-	print(pet)
